94b9b922e306a0543b1b10add909ab4ff1696618
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-06-26 22:06:36 +0200

    Clear project for new solution

--- passleak/main.py
+++ passleak/main.py
@@ -1,12 +1,1 @@
 """PassLeak main"""
-import logging
-from password_validator import PasswordValidator
-
-
-logging.basicConfig(level=logging.INFO)
-
-PasswordValidator.load_passwords()
-PasswordValidator.validate()
-PasswordValidator.validate_leaks()
-logging.info(PasswordValidator.show_all_passwords())
-PasswordValidator.save_safety_password()
